316c932442121d16e343ccaf281c948afc790573
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2022-01-09 01:51:13 -0600

    :upside_down:

--- src/main/java/dev/lazurite/fabatik/FabatikClient.java
+++ src/main/java/dev/lazurite/fabatik/FabatikClient.java
@@ -1,11 +1,8 @@
 package dev.lazurite.fabatik;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
-public class FabatikClient implements ClientModInitializer {
+public final class FabatikClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
